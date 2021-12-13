b903f32850481a7468931463ddfefa31f2e396ba
Edward cicio <edwardcicioc@gmail.com>
2021-12-12 19:28:58 -0500

    Modify Method

--- Project_0App/Project_0App.App/Login.cs
+++ Project_0App/Project_0App.App/Login.cs
@@ -8,26 +8,37 @@ namespace Project_0App.App
 {
     public class Login
     {
-        private string? _username { get; set; }
-        private string? _password { get; set; }
-
-        public void LoginScreen()
+        // Modify Switch statment in Dec 11, 2021
+        public MainProgram.Mode LoginScreen(MainProgram.Mode myMode)
         {
             int input;
-            Console.WriteLine("Please choose a number from menu:\n1. New Customer\n2. Returned Customer\n3. Manager");
-            if(int.TryParse(Console.ReadLine(), out input))
+            bool TryAgain = false;
+
+            do
             {
-                switch (input)
+                Console.WriteLine("Please choose a number from menu:\n1. New Customer\n2. Returned Customer\n3. Manager\n4. Exit");
+                if (int.TryParse(Console.ReadLine(), out input))
                 {
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
+                    switch (input)
+                    {
+                        case 1: return MainProgram.Mode.NewCustomer;
+                        case 2: return MainProgram.Mode.ReturnedCustomer;
+                        case 3: return MainProgram.Mode.Manager;
+                        case 4: return MainProgram.Mode.Exit;
+                        default: 
+                            TryAgain = true;
+                            Console.WriteLine("Your input don't mathc any menu. Please try again.\n");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your input is not number. Please try again.\n");
+                    TryAgain = true;
                 }
             }
-            else
-            {
-                throw new NotImplementedException();
-            }
+            while (TryAgain);
+            return MainProgram.Mode.Exit;
         }
     }
 }
