2a1116f75296177498e5a72d7211d4c553c9f04a
Edward cicio <edwardcicioc@gmail.com>
2021-12-12 19:31:39 -0500

    Just add WriteLine

--- Project_0App/Project_0App.App/ReturnedCustomer.cs
+++ Project_0App/Project_0App.App/ReturnedCustomer.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Project_0App.App
 {
-    public class ReturnedCustomer
+    public class ReturnedCustomer : Person
     {
+        public string? _Username { get; set; }
+        public string? _Password { get; set; }
+        public int _ID { get; set; }
+
+
+
+        // Add Method in Dec 11, 2021
+        public void EnterReturnedCustomer()
+        {
+            Console.WriteLine("Test in ReturnedCustomer.cs");
+            throw new NotImplementedException();
+        }
+
+        public void DisplayStoreOrderHistory()
+        {
+
+        }
     }
 }
