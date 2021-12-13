2f1a472b18bf30a78fa1040e6f37b64718b243fd
Edward cicio <edwardcicioc@gmail.com>
2021-12-12 19:33:04 -0500

    try to get username in same class

--- Project_0App/Project_0App.App/Manager.cs
+++ Project_0App/Project_0App.App/Manager.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Project_0App.App
 {
-    public class Manager
+    public class Manager : Person
     {
+        public string? _Username { get; }
+        public string? _Password { get; }
+        public int _ID { get; set; }
+
+        public Manager(string username, string password)
+        {
+            _Username = username;
+            _Password = password;
+        }
+
+        // Add in Dec 11, 2021
+        public void EnterManager()
+        {
+            Console.WriteLine("Test in Manager.cs");
+            throw new NotImplementedException();
+        }
+
+        static void matchUserPass(string? userName, string? password)
+        {
+            if(userName != null && password != null)
+            {
+                if(userName.Equals(_Username) )
+                {
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Your username or password are empty");
+            }
+
+        }
+
+        public void DisplayStoreOrderHistory()
+        {
+
+        }
     }
 }
