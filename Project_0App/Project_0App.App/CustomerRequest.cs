6f54f7f05fdd36d6a817949cb1d4340ac6db9833
Edward cicio <edwardcicioc@gmail.com>
2021-12-13 22:57:52 -0500

    Add Switch statement

--- Project_0App/Project_0App.App/CustomerRequest.cs
+++ Project_0App/Project_0App.App/CustomerRequest.cs
@@ -8,5 +8,51 @@ namespace Project_0App.App
 {
     public class CustomerRequest
     {
+        public void EnterCustomerRequest(ref MainProgram.Mode myMode)
+        {
+            int input;
+            bool TryAgain = false;
+
+            do
+            {
+                Console.WriteLine("Please choose a number from menu:\n1. Check your order hisoty\n2. See item's information" +
+                                  "\n3. Check store history\n4. Ready to order items\n5. Exit");
+                if (int.TryParse(Console.ReadLine(), out input))
+                {
+                    switch (input)
+                    {
+                        case 1:
+                            // Go to CustomerOrderHistory.cs
+                            break;
+                        case 2:
+                            // Got to OrderDetail.cs
+                            break;
+                        case 3: 
+                            // Go to StoreOrderHistory.cs
+                            break;
+                        case 4:
+                            // Go to SetOrder.cs
+                            myMode = MainProgram.Mode.SetOrder;
+                            break;
+                        case 5:
+                            // Exit
+                            myMode = MainProgram.Mode.Exit;
+                            break;
+                        default:
+                            TryAgain = true;
+                            Console.WriteLine("Your input don't mathc any menu. Please try again.\n");
+                            break;
+                    }
+                    if (input == 4 || input == 5)
+                        TryAgain = false;
+                }
+                else
+                {
+                    Console.WriteLine("Your input is not number. Please try again.\n");
+                    TryAgain = true;
+                }
+            }
+            while(TryAgain);
+        }
     }
 }
