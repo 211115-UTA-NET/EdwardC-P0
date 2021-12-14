964f370145342d3831678196216227df9033bae7
Edward cicio <edwardcicioc@gmail.com>
2021-12-13 23:00:03 -0500

    Add new xUnit test

---
+++ Project_0App/Project_0App.Tests/NewCustomerTest.cs
@@ -0,0 +1,18 @@
+﻿using Project_0App.App;
+using Xunit;
+
+namespace Project_0App.Tests;
+public class NewCustomerTest
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        CustomerRequest customerRequest = new CustomerRequest();
+
+        // Act
+
+        // Assert
+    }
+}
+
