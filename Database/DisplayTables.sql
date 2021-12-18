7063ac4678271cbd79a2e40ddcac9d5ea2c2d5bc
Edward cicio <edwardcicioc@gmail.com>
2021-12-18 15:01:47 -0500

    Add more display syntax

--- Database/DisplayTables.sql
+++ Database/DisplayTables.sql
@@ -12,3 +12,15 @@ SELECT * FROM ItemDetails;
 SELECT InvoiceHistory.InvoiceId, InvoiceDate, InvoiceList.ItemName, InvoiceList.ItemQuanity, InvoiceList.TotalPrice
 FROM InvoiceHistory
 FULL OUTER JOIN InvoiceList ON InvoiceList.InvoiceId = InvoiceHistory.InvoiceId;
+
+
+-- Display Customer Order History
+SELECT InvoiceHistory.InvoiceId, InvoiceDate, InvoiceList.ItemName, InvoiceList.ItemQuanity, InvoiceList.TotalPrice, StoreLocations.StoreLocation
+FROM InvoiceHistory
+INNER JOIN InvoiceList On InvoiceList.InvoiceId = InvoiceHistory.InvoiceId
+INNER JOIN Invoices ON Invoices.InvoiceId = InvoiceList.InvoiceId
+INNER JOIN StoreLocations On StoreLocations.StoreId = Invoices.StoreId
+INNER JOIN Customers ON Customers.CustomerId = Invoices.CustomerId;
+
+-- Display details of an Order
+SELECT * FROM ItemDetails;
