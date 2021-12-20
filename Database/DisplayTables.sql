9c31efbc0acd5c7e56439d2b56859aae3949a636
Edward cicio <edwardcicioc@gmail.com>
2021-12-19 23:26:49 -0500

    Add more Display

--- Database/DisplayTables.sql
+++ Database/DisplayTables.sql
@@ -24,3 +24,46 @@ INNER JOIN Customers ON Customers.CustomerId = Invoices.CustomerId;
 
 -- Display details of an Order
 SELECT * FROM ItemDetails;
+
+-- Display StoreItems 
+SELECT StoreItems.ItemName, StoreItems.ItemQuantity, StoreLocations.StoreLocation
+FROM StoreItems
+INNER JOIN StoreLocations ON StoreLocations.StoreId = StoreItems.StoreId
+WHERE StoreLocations.StoreId = 2;
+
+-- Display Process Order
+SELECT ItemName, Price 
+FROM ItemDetails
+
+-- CheckQuantityItem
+SELECT ItemName, ItemQuantity
+FROM StoreItems
+WHERE StoreId = 1 AND ItemName = 'Shimano Altus FC-M311 Crankset'
+
+--Display ItemName
+SELECT ItemName
+FROM ItemDetails
+
+-- Display Invoice for Customer and store History
+SELECT Invoices.InvoiceId, InvoiceHistory.InvoiceDate, 
+	InvoiceList.ItemName, InvoiceList.ItemQuanity, 
+	InvoiceList.TotalPrice, StoreLocations.StoreLocation
+FROM Invoices
+INNER JOIN InvoiceHistory ON InvoiceHistory.InvoiceId = Invoices.InvoiceId
+INNER JOIN InvoiceList ON InvoiceList.InvoiceId = Invoices.InvoiceId
+INNER JOIN StoreLocations On StoreLocations.StoreId = Invoices.StoreId
+WHERE Invoices.CustomerId = 1;
+
+-- Display Store Items for Manager to check Inventory
+SELECT ItemId, ItemName, ItemQuantity, StoreLocation
+FROM StoreItems
+INNER JOIN StoreLocations ON StoreLocations.StoreId = StoreItems.StoreId;
+
+-- Test for add Invoices, InvoiceHistory, and InvoicesList
+SELECT * FROM Invoices;
+SELECT * FROM InvoiceHistory;
+SELECT * FROM InvoiceList;
+
+SELECT TOP 1 InvoiceId
+FROM Invoices
+ORDER BY InvoiceId DESC;
