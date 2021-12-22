e99ec891f91425b6795f0bd781ed43c2ce6f2e4e
Edward cicio <edwardcicioc@gmail.com>
2021-12-21 22:36:41 -0500

    Add various records for tables

--- Database/Set Records.sql
+++ Database/Set Records.sql
@@ -31,9 +31,13 @@ VALUES
 INSERT StoreItems
     (ItemName, StoreId, ItemQuantity)
 VALUES
-    ('Shimano Altus FC-M311 Crankset', 1, 5),
-    ('Shimano Altus FC-M311 Crankset', 2, 6),
-    ('Shimano Altus FC-M311 Crankset', 3, 3)
+    ('Shimano Altus FC-M311 Crankset', 1, 17),
+    ('Shimano Altus FC-M311 Crankset', 2, 9),
+    ('Shimano Altus FC-M311 Crankset', 3, 5),
+	('Odyssey Bluebird Single Speed', 1, 10),
+	('Odyssey Bluebird Single Speed', 2, 11),
+	('Lizard Skins DSP Bar Tape V2 4.6mm', 1, 15),
+	('Lizard Skins DSP Bar Tape V2 4.6mm', 3, 8)
 
 -- UPDATE StoreItems
 -- SET ItemId = 1
@@ -61,14 +65,32 @@ VALUES
 
 --Add Invoices, InvoiceHistory, InvoiceList
 
-INSERT Invoices (CustomerId, StoreId)
-VALUES (1, 2);
+INSERT Invoices 
+	(CustomerId, StoreId)
+VALUES 
+	(1, 2),
+	(1, 1),
+	(1, 3),
+	(1, 2),
+	(1, 2);
 
-INSERT InvoiceHistory (InvoiceId, InvoiceDate)
-VALUES (1, '2000-04-25')
+INSERT InvoiceHistory 
+	(InvoiceId, InvoiceDate)
+VALUES 
+	(1, '2000-04-25'),
+	(2, '1994-09-01'),
+	(3, '2003-08-17'),
+	(4, '2004-01-20'),
+	(5, '2017-03-14')
 
-INSERT InvoiceList (InvoiceId, ItemName, ItemQuantity, TotalPrice)
-VALUES (1, 'Shimano Altus FC-M311 Crankset', 3, 164)
+INSERT InvoiceList 
+	(InvoiceId, ItemName, ItemQuantity, TotalPrice)
+VALUES 
+	(1, 'Shimano Altus FC-M311 Crankset', 3, 164.97),
+	(2, 'Odyssey Bluebird Single Speed', 2, 31.98),
+	(3, 'Lizard Skins DSP Bar Tape V2 4.6mm', 4, 207.96),
+	(4, 'Odyssey Bluebird Single Speed', 4, 63.96),
+	(5, 'Lizard Skins DSP Bar Tape V2 4.6mm', 2, 103.98);
 
 -- Modify the store's inventory decrease
 UPDATE StoreItems
