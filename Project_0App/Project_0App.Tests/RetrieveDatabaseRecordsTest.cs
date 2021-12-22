b648c600abfe86f939c590b8b5708d8df1b1f849
Edward cicio <edwardcicioc@gmail.com>
2021-12-21 22:40:36 -0500

    Add Unit test for retrieve

---
+++ Project_0App/Project_0App.Tests/RetrieveDatabaseRecordsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Project_0App.Tests
+{
+    public class RetrieveDatabaseRecordsTest
+    {
+        [Fact]
+        public void Success_getFinalPrice()
+        {
+            // Arrange
+
+
+            // Act
+
+            // Assert
+        }
+    }
+}
