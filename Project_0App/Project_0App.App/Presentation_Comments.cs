183944fef1f02e29245cd212b7d706c7df70b759
Edward cicio <edwardcicioc@gmail.com>
2021-12-21 22:44:02 -0500

    Use this file to communicate for group

---
+++ Project_0App/Project_0App.App/Presentation_Comments.cs
@@ -0,0 +1,216 @@
+﻿
+
+
+
+
+
+
+// Hello, my name is Edward Cicio. I will use this comment
+// to guide you through my presentation. 
+
+
+
+
+
+
+
+// My project talk about Bike Store that sell bike parts. Therefore my target audience is
+// any age range who love to ride bike. Even cycle biker, road biker, and Mountain biker too.
+// Even this will help Store owner to manage their store. This app have access with Azura Database,
+// where you can store lots of information includes invoices of customer order or store invoices.
+// Before we go ahead to show our app.
+
+
+
+
+
+
+
+// I want to make sure that you are aware of Mode. 
+// When I say Login mode, New Customer Mode, etc.… It is basic guide to help the
+// program know where to go 
+
+
+
+
+
+
+
+// Here is the Step Instructions.
+// 1. When we open the app, we will enter Login Screen (Login Mode). The app will ask us to choose
+//    either New Customer, Returned Customer (or Manager), or Exit(Close the app).
+
+
+
+
+
+
+
+// 2. First, we will choose New Customer (Mode). The program will inform us to fill out the
+//    information followed by First Name, Last Name, Phone Number (up to 10 digit), Address,
+//    username, and password.
+
+
+
+
+
+
+
+// 3. After we finish fill the information, we will enter Customer Request Mode, where we will choose a
+//    store location to shopping.
+
+
+
+
+
+
+
+// 4. After pick a location, then we will have few choices from menu: View your Invoices (your order history),
+//    Item’s details, View store’s Invoice (Store History), Ready to order, and Exit.
+
+
+
+
+
+
+
+// 5. First, My Invoices (Which will be empty due to new customer).
+//    Secondly, Item’s detail.
+//    Thirdly, Store’s invoices based on location.
+//       By normal, for customer to look store's invoices. I plan to
+//       add most popular item on top and unpopular on bottom in the future development.
+//       This idea will help the customers to decide which item to buy or how many quantities
+//       they may need. Include order date to know which season is better to buy.
+//    And lastly, Ready to order (Change to Set Order Mode).
+
+
+
+
+
+
+
+// 6. The program will display the store inventory based on the location that we choose from Customer
+//    Request. After that, the program will prompt us to choose which item and how many quantities
+//    that we want to order. 
+
+
+
+
+
+
+
+// 7. Even if we order more quantity than the store’s quantity. Then the program will reject our order and
+//    ask us to input again. 
+
+
+
+
+
+
+
+// 8. Once approved our order, the program will add our order in our list. Then the program
+//    will ask us to process (Change to Process Order Mode) or continue order
+//    (Add another item into our cart).
+
+
+
+
+
+
+
+// 9. When we type ‘process’, the program will prompt us to final confirm.
+//    It is same as we see the total price, then decide to go ahead pay or cancel the order.
+
+
+
+
+
+
+
+// 10. At first, the program will display our list of order and final price.
+//     Then prompt us to input either ‘yes’ or ‘no’
+
+
+
+
+
+
+
+// 11. If we choose ‘yes’, then the program will start process our order (Also update our Invoices)
+//     and change to Logout Mode. If we choose ‘no’, then the program will cancel our order and change
+//     to Logout Mode. 
+
+
+
+
+
+
+
+// 12. After choosing ‘yes’ or ‘no’, the program will prompt us to logout or continue shopping
+
+
+
+
+
+
+
+// 13. If we choose to log out, then the program will change into Login Mode.
+//     If we choose to continue shopping, then the program will change to Customer Request Mode.
+
+
+
+
+
+
+
+// 14. For now, we will choose to continue shopping to show you that our cart is added
+//     into our invoices and store’s invoice. After this, I will do choose Exit to logout
+//     then login as Manager.
+
+
+
+
+
+
+
+// 15. When we login as Manager, the program will change into Manager Request Mode,
+//     instead Customer Request as last time.
+
+
+
+
+
+
+
+// 16. As Manager,
+//     1) check our store’s invoice (see history either our choose
+//     location or display all store.)
+//     2) check our store’s inventory based on location
+//     or all stores.
+//     3) search customer's first name in our database system.
+//     After that, we just choose to log out.
+
+
+
+
+
+
+
+// 17. We are done with app. 
+
+
+
+
+
+
+
+// For database, I have 8 Tables in total. I have a diagram in SQL Management.
+
+
+
+
+
+
+
+// Any questions you may have for me? Please comment your question in the chat,
+// so I can answer your questions.
